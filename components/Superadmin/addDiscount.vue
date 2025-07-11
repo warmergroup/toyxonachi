661244d758d82db7e26040df33325c832057fab8
warmergroup <ulugbekjonsultonov4@gmail.com>
2025-07-12 04:51:33 +0500

    update addDiscount component

--- components/Superadmin/addDiscount.vue
+++ components/Superadmin/addDiscount.vue
@@ -87,7 +87,7 @@ function handleSave() {
 <template>
     <div class="flex flex-col items-center justify-center w-full">
         <label v-if="!imageUrl" for="dropzone-file"
-            class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+            class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
             <div class="flex flex-col items-center justify-center pt-5 pb-6">
                 <svg v-if="!uploading" class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
